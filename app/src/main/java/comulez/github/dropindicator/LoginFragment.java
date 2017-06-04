59e79df5ff69c6d11007b0b30505821bb1bedc60
Xudanli2809 <1412421134@qq.com>
2017-06-04 14:15:57 +0800

    Initial commit

---
+++ app/src/main/java/comulez/github/dropindicator/LoginFragment.java
@@ -0,0 +1,46 @@
+package comulez.github.dropindicator;
+
+/**
+ * Created by ASUS on 2017/5/22.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginFragment extends Fragment {
+    private EditText et_name;
+    private EditText et_pw;
+    private Button register;
+    private Button password;
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.login,container,false);
+        return v;
+    }
+
+
+
+
+
+}
+
+
+
